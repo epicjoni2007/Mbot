69756e7d97881fca07af9a27db47c89ec87fb702
amarjul <145123242+amarjul@users.noreply.github.com>
2025-01-30 08:30:02 +0100

    Update index.js

--- Frontend/TestAPI/index.js
+++ Frontend/TestAPI/index.js
@@ -4,10 +4,6 @@ const port = 3000
 
 app.use(express.json())
 
-app.get('/test', (req, res) => {
-  res.send('It works!')
-})
-
 app.get('/sensor', (req, res) => {
   res.send(JSON.stringify({
     wandEntfernung: ((Math.random() * (2 - 1) + 1).toFixed(2)),
@@ -25,7 +21,7 @@ app.get('/daten', (req, res) => {
 app.get('/ip', (req, res) => {
   const ips = ['10.10.3.14', '192.168.0.133', '172.16.1.34']
   const randIP = ips[Math.floor(Math.random() * ips.length)]
-  const name = ['Ernest', 'Beefcake', 'Fleischkuchn', 'Beefei', 'Shishei']
+  const name = ['mbot1', 'mbot2', 'mbot3', 'mbot4']
   const randName = name[Math.floor(Math.random() * name.length)]
 
   res.send(JSON.stringify({

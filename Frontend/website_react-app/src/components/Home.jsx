4e1a4a9195a932c51582d7a15dd7acbdc8ac8ca1
amarjul <145123242+amarjul@users.noreply.github.com>
2025-02-26 10:33:05 +0100

    Home and Joystick added, Home faulty

--- Frontend/website_react-app/src/components/Home.jsx
+++ Frontend/website_react-app/src/components/Home.jsx
@@ -1,7 +1,41 @@
-const Home = () => {
+import { useState, useEffect } from "react"
+import "../css/Home.css"
+import icon from "/main-icon.png"
+
+const Home = (props) => {
+    const[BetrWeg, setBetrWeg] = useState([])
+
+    useEffect(() => {
+        fetch('http://' + props.IpName[0] + ':3000/daten')
+        .then(response => response.json())
+        .then(data => setBetrWeg([
+            data.betriebsDauer,
+            data.wegStrecke
+        ]))
+        return
+      }, [])
+
     return(
         <div id="home">
-            HOME
+            <div id="datatabs">
+                <div className="datatab">
+                    <div className="datatabtext">
+                        IP-Adresse: {props.IpName[0]}
+                    </div>
+                    <div className="datatabtext">
+                        Name: {props.IpName[1]}
+                    </div>
+                </div>
+                <div className="datatab">
+                    <div className="datatabtext">
+                        Gefahrene Strecke: {BetrWeg[0]}
+                    </div>
+                    <div className="datatabtext">
+                        Betriebsdauer: {BetrWeg[1]}
+                    </div>
+                </div>
+                <img id="homemainicon" src={icon} alt="MBot Icon" />
+            </div>
         </div>
     )
 }
--- Frontend/website_react-app/src/components/Joystick.jsx
+++ Frontend/website_react-app/src/components/Joystick.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import "../css/Joystick.css"
+
 const Joystick = () => {
-    return(
-        <div id="joystick">
-            JOYSTICK
+  const [speed, setSpeed] = useState(10);
+
+  return (
+    <div className="container">
+      <div className="left">
+        <div className="speed">
+          <label className="inputSpeed">Geschwindigkeit: {speed}</label>
         </div>
-    )
+      
+        <input
+            className="slider"
+            type="range"
+            min="0"
+            max="100"
+            step="1"
+            value={speed}
+            onChange={(e) => setSpeed(parseInt(e.target.value))}
+          />
+
+        <div className="arrows">
+          <i className="uil uil-arrow-circle-up"></i>
+          <i className="uil uil-arrow-circle-right"></i>
+          <i className="uil uil-arrow-circle-down"></i>
+          <i className="uil uil-arrow-circle-left"></i>
+          <i className="uil uil-stop-circle"></i>
+        </div>
+      </div>
+
+      <div className="right">
+        <img className="coban" src="coban.png" alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Joystick
